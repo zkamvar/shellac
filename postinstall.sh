a3bf1027d7011e86d7020d9f17cd505eefcbf248
Zhian N. Kamvar <zkamvar@gmail.com>
2022-01-04 10:05:33 -0800

    assets -> source

--- postinstall.sh
+++ postinstall.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# after npm install, copy assets into directories that we can compile from
+# after npm install, copy source into directories that we can compile from
 set -eux pipefail
-rm -rf assets/stylesheets/bootstrap
-cp -rf node_modules/bootstrap assets/stylesheets/
-rm -rf assets/stylesheets/fontawesome
+rm -rf source/stylesheets/bootstrap
+cp -rf node_modules/bootstrap source/stylesheets/
+rm -rf source/stylesheets/fontawesome
 
-rm -rf assets/javascripts/vendor
-mkdir -p assets/javascripts/vendor/jquery
-mkdir -p assets/javascripts/vendor/jquery-visible
-mkdir -p assets/javascripts/vendor/bootstrap
-mkdir -p assets/javascripts/vendor/feather-icons/
-#mkdir -p assets/javascripts/vendor/popper
-cp -rf node_modules/jquery/dist/* assets/javascripts/vendor/jquery/
-cp -rf node_modules/jquery-visible/* assets/javascripts/vendor/jquery-visible/
-cp -rf node_modules/bootstrap/dist/js/* assets/javascripts/vendor/bootstrap/
-cp -rf node_modules/feather-icons/dist/* assets/javascripts/vendor/feather-icons/
+rm -rf source/javascripts/vendor
+mkdir -p source/javascripts/vendor/jquery
+mkdir -p source/javascripts/vendor/jquery-visible
+mkdir -p source/javascripts/vendor/bootstrap
+mkdir -p source/javascripts/vendor/feather-icons/
+#mkdir -p source/javascripts/vendor/popper
+cp -rf node_modules/jquery/dist/* source/javascripts/vendor/jquery/
+cp -rf node_modules/jquery-visible/* source/javascripts/vendor/jquery-visible/
+cp -rf node_modules/bootstrap/dist/js/* source/javascripts/vendor/bootstrap/
+cp -rf node_modules/feather-icons/dist/* source/javascripts/vendor/feather-icons/
 
 
-#cp -rf node_modules/popper.js/dist/* assets/javascripts/vendor/popper/
+#cp -rf node_modules/popper.js/dist/* source/javascripts/vendor/popper/
